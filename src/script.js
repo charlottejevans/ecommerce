ea60ed30f871df8116074fcc5cb7bb9f3f60a885
Charlotte Evans <charlotteevans543@gmail.com>
2024-05-02 14:19:06 +0200

    Created variables: 'minusQuantityBtn' Created if statement to check whether the number of items in cart is less than 0.

--- src/script.js
+++ src/script.js
@@ -1,16 +1,27 @@
 
 
-let addQuantityBtn, quantityDisplay;
+let addQuantityBtn, quantityDisplay, minusQuantityBtn;
 let quantityNumber = 0;
 
     function onInit() {
-        addQuantityBtn = document.getElementById('addQuantity');
+        addQuantityBtn = document.getElementById('addQuantityBtn');
         quantityDisplay = document.getElementById('quantityNumber');
+        minusQuantityBtn = document.getElementById('minusQuantityBtn');
 
         addQuantityBtn.onclick = () => {
             quantityNumber += 1;
             quantityDisplay.innerHTML = quantityNumber;
         };
+
+        minusQuantityBtn.onclick = () => {
+            quantityNumber -= 1;
+            quantityDisplay.innerHTML = quantityNumber;
+
+            if (quantityNumber < 0) {
+                quantityNumber = 0;
+                quantityDisplay.innerHTML = quantityNumber;
+            }
+        }
     }
 
     window.onload = onInit;
